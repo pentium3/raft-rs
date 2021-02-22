02ef8d01ea0c0ce5b0cfc5a699db88b982c89d05
Andrew Yoo <abyoo2@illinois.edu>
2021-02-22 13:56:10 -0800

    fixed logger

--- src/raw_node.rs
+++ src/raw_node.rs
@@ -288,7 +288,7 @@ impl<T: Storage> RawNode<T> {
         e.context = context;
         m.set_entries(vec![e].into());
         
-        info!(self.logger, "proposing here : {} {} {} {:?}", m.to, m.term, m.index, m.get_msg_type());  //MsgAppend
+        info!(rn.raft.logger, "proposing here : {} {} {} {:?}", m.to, m.term, m.index, m.get_msg_type());  //MsgAppend
         self.raft.step(m)
     }
 
